bbceb05f721b82fcdb0d2b227dfff4a6b78f827a
flocknroll <dosso.florent@gmail.com>
2017-03-29 17:11:20 +0200

    H8 end

--- H8/Party.hs
+++ H8/Party.hs
@@ -1,6 +1,7 @@
 module Party where
 
 import Data.Tree
+import Data.List
 import Employee
 
 glCons :: Employee -> GuestList -> GuestList
@@ -15,8 +16,34 @@ moreFun l@(GL _ f) l'@(GL _ f') = if f > f' then l else l'
 
 ---------------------------------------------------------------
 
-treeFold :: b -> ([b] -> a -> b) -> Tree a -> b
+treeFold :: b -> (a -> [b] -> b) -> Tree a -> b
 treeFold e f (Node val subTree) = let subFold = treeFold e f
-                                    in f (map subFold subTree) val
+                                    in f val (map subFold subTree)
 
 ----------------------------------------------------------------------
+
+nextLevel :: Employee -> [(GuestList, GuestList)]
+                -> (GuestList, GuestList)
+nextLevel e l = let maxFunBoss  = foldl (\a (_, g)  -> a `mappend` g)                mempty l
+                    maxFunOther = foldl (\a (g, g') -> (a `mappend` (moreFun g g'))) mempty l
+                in (glCons e maxFunBoss, maxFunOther)
+
+--------------------------------------------------------------------------------
+
+maxFun :: Tree Employee -> GuestList
+maxFun t = let (g, g') = treeFold mempty nextLevel t
+            in moreFun g g'
+
+---------------------------------------------------------------
+
+getNames :: [Employee] -> String
+getNames = unlines . sort . map empName
+
+showGuestList :: GuestList -> String
+showGuestList (GL e f) = "Total fun: " ++ show f ++ "\n" ++ getNames e
+
+main :: IO ()
+main = do
+        file <- readFile "company.txt"
+        let list = showGuestList . maxFun . read $ file
+        putStrLn list
