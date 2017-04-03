ed33ad06d75d810a1cc80c59a9a0521d0f5db693
flocknroll <dosso.florent@gmail.com>
2017-04-03 18:05:50 +0200

    H12 ex 2

--- H12/Risk.hs
+++ H12/Risk.hs
@@ -3,6 +3,7 @@
 module Risk where
 
 import Control.Monad.Random
+import Data.List
 
 ------------------------------------------------------------
 -- Die values
@@ -25,4 +26,36 @@ die = getRandom
 
 type Army = Int
 
-data Battlefield = Battlefield { attackers :: Army, defenders :: Army }
+data Battlefield = Battlefield { attackers :: Army, defenders :: Army } deriving (Show)
+
+instance Monoid Battlefield where
+  mempty = Battlefield 0 0
+  mappend b1 b2 = Battlefield (attackers b1 + attackers b2) (defenders b1 + defenders b2)
+
+maxAtk :: Battlefield -> Int
+maxAtk bf = min 3 (attackers bf - 1)
+
+maxDef :: Battlefield -> Int
+maxDef bf = min 2 (defenders bf)
+
+makeRolls :: Int -> Rand StdGen [Int]
+makeRolls 0 = return []
+makeRolls n = do
+                d <- die
+                r <- makeRolls (n - 1)
+                return $ (unDV d) : r
+
+calcDiff :: [Int] -> [Int] -> Battlefield
+calcDiff a d = let  sa = reverse (sort a)
+                    sd = reverse (sort d)
+                    res = zipWith (\a d -> if a > d then Battlefield 0 (-1) else Battlefield (-1) 0) sa sd
+                in foldl (\a b -> a `mappend` b) mempty res
+
+battle :: Battlefield -> Rand StdGen Battlefield
+battle bf = do
+              let atk = maxAtk bf
+                  def = maxDef bf
+              rollsAtk <- makeRolls atk
+              rollsDef <- makeRolls def
+              let res = calcDiff rollsAtk rollsDef
+              return $ bf `mappend` res
