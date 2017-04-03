0434baefc43b3629fb45654abcc21ee5d6d3f3b0
flocknroll <dosso.florent@gmail.com>
2017-04-03 14:45:38 +0200

    H11 : ex 1 & 2

--- H11/SExpr.hs
+++ H11/SExpr.hs
@@ -6,26 +6,27 @@ module SExpr where
 
 import AParser
 import Control.Applicative
+import Data.Char
 
 ------------------------------------------------------------
 --  1. Parsing repetitions
 ------------------------------------------------------------
 
 zeroOrMore :: Parser a -> Parser [a]
-zeroOrMore p = undefined
+zeroOrMore p = oneOrMore p <|> pure []
 
 oneOrMore :: Parser a -> Parser [a]
-oneOrMore p = undefined
+oneOrMore p = liftA2 (:) p (zeroOrMore p)
 
 ------------------------------------------------------------
 --  2. Utilities
 ------------------------------------------------------------
 
 spaces :: Parser String
-spaces = undefined
+spaces = zeroOrMore (satisfy isSpace)
 
 ident :: Parser String
-ident = undefined
+ident = liftA2 (:) (satisfy isAlpha) (zeroOrMore (satisfy isAlphaNum)) 
 
 ------------------------------------------------------------
 --  3. Parsing S-expressions
